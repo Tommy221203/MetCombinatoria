94e5d10ce0babdcfc64e8d330c589e8efe616ba4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-24 13:27:41 +0100

    mancal'ultimo test

--- CalcoliCombinatori.Test/UnitTest1.cs
+++ CalcoliCombinatori.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CalcoliCombinatori.Test
@@ -12,6 +13,38 @@ namespace CalcoliCombinatori.Test
             long n = 0;
             long ris = EquazioniLibrary.CalcoliCombinatori.Fattoriale(n);
             long risasp = 1;
+            Assert.AreEqual(risasp, ris);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            long n = 1;
+            long ris = EquazioniLibrary.CalcoliCombinatori.Fattoriale(n);
+            long risasp = 1;
+            Assert.AreEqual(risasp, ris);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            long n = 5;
+            long ris = EquazioniLibrary.CalcoliCombinatori.Fattoriale(n);
+            long risasp = 120;
+            Assert.AreEqual(risasp, ris);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            long n = 10;
+            long ris = EquazioniLibrary.CalcoliCombinatori.Fattoriale(n);
+            long risasp = 3628800;
+            Assert.AreEqual(risasp, ris);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            long n = 50;
+            long ris = EquazioniLibrary.CalcoliCombinatori.Fattoriale(n);
+            BigInteger risasp = new BigInteger ();
             Assert.AreEqual(risasp, ris);
         }
     }
